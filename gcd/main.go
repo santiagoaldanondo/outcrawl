3c37ea8ccfbdc55175829e05c9b257ec16f1b2f5
santiagoaldanondo <santiagoaldanondo@gmail.com>
2019-07-31 00:18:19 +0200

    fixes to github path and go version

--- gcd/main.go
+++ gcd/main.go
@@ -1,32 +1,36 @@
 package main
+
 import (
-  "log"
-  "net"
-  "github.com/santiagoaldanondo/outcrawl"
-  context "golang.org/x/net/context"
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"github.com/santiagoaldanondo/outcrawl/pb"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct{}
 
 func main() {
-  lis, err := net.Listen("tcp", ":3000")
-  if err != nil {
-    log.Fatalf("Failed to listen: %v", err)
-  }
-  s := grpc.NewServer()
-  pb.RegisterGCDServiceServer(s, &server{})
-  reflection.Register(s)
-  if err := s.Serve(lis); err != nil {
-    log.Fatalf("Failed to serve: %v", err)
-  }
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGCDServiceServer(s, &server{})
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
 
 func (s *server) Compute(ctx context.Context, r *pb.GCDRequest) (*pb.GCDResponse, error) {
-  a, b := r.A, r.B
-  for b != 0 {
-    a, b = b, a%b
-  }
-  return &pb.GCDResponse{Result: a}, nil
+	a, b := r.A, r.B
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return &pb.GCDResponse{Result: a}, nil
 }
