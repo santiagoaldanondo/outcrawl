2b3d136cf0d859759bc3b874753562dd0005aca9
santiagoaldanondo <santiagoaldanondo@gmail.com>
2019-07-30 23:19:00 +0200

    complete tutorial

--- pb/main.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-  "log"
-  "net"
-  "github.com/santiagoaldanondo/outcrawl"
-  context "golang.org/x/net/context"
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/reflection"
-)
