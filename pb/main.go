00b763d7acef57dc03c46a48e899813a8383a7d1
santiagoaldanondo <santiagoaldanondo@gmail.com>
2019-07-30 22:54:15 +0200

    starting tutorial from https://outcrawl.com/getting-started-microservices-go-grpc-kubernetes

---
+++ pb/main.go
@@ -0,0 +1,9 @@
+package main
+import (
+  "log"
+  "net"
+  "github.com/santiagoaldanondo/outcrawl"
+  context "golang.org/x/net/context"
+  "google.golang.org/grpc"
+  "google.golang.org/grpc/reflection"
+)
